a603dbad4b6e4f04c927cfdabc0cca3191c5ad29
Kshitij Jain <jainkshitij1530@gmail.com>
2023-09-16 19:13:36 +0530

    Client Side Library Update

--- Whatsapp.Clone/Areas/Identity/IdentityHostingStartup.cs
+++ Whatsapp.Clone/Areas/Identity/IdentityHostingStartup.cs
@@ -1,6 +1,14 @@
-﻿namespace Whatsapp.Clone.Areas.Identity
+﻿
+[assembly: HostingStartup(typeof(Whatsapp.Clone.Areas.Identity.IdentityHostingStartup))]
+
+namespace Whatsapp.Clone.Areas.Identity
 {
-    public class IdentityHostingStartup
+    public class IdentityHostingStartup : IHostingStartup
     {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
     }
 }
