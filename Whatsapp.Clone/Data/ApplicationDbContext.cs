05bccc465a0bcbfdd17558dc7c68dfb406b2a1e7
Kshitij Jain <jainkshitij1530@gmail.com>
2023-09-17 17:50:33 +0530

    Adding Logic Controllers

--- Whatsapp.Clone/Data/ApplicationDbContext.cs
+++ Whatsapp.Clone/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using Whatsapp.Clone.Models;
 
 namespace Whatsapp.Clone.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
