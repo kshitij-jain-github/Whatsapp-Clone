5a1be47e0f5745b712c0167e775077aaaf07fddf
Kshitij Jain <jainkshitij1530@gmail.com>
2023-09-17 17:43:06 +0530

    Identity Setup

---
+++ Whatsapp.Clone/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Whatsapp.Clone.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
